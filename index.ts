91533f1e736a1884e0d292658078d2ce13addbc8
desperado <youtubhexo@gmail.com>
2024-09-14 16:57:48 +0800

    ts类型介绍

---
+++ index.ts
@@ -0,0 +1,34 @@
+
+//基础类型
+//string number boolean symbol null undefined
+
+
+const msg:string='AI赋能开发'
+
+const msg1:number =1
+
+
+let  msg3:boolean=true
+
+
+
+
+//引用类型
+
+//{} or Object   [] or Array   function class
+
+
+let msg4:{}={}
+let msg5:{"name":string,"age":number}={"name":"张三","age":18}
+
+let msg6:Object={"name":"张三","age":18}
+
+let msg7:[]=[]
+let msg8:(string|number)[]=[1,'1']
+
+let msg9:Array<string|number>=[1,'1']
+
+
+
+
+
